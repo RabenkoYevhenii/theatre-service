66819911edb4e5077af9d2817f0eed31a6a94e21
Eugene Rabenko <qwer09846@gmail.com>
2023-12-11 22:41:08 +0200

    add swagger & redoc

--- theatre_service_api/urls.py
+++ theatre_service_api/urls.py
@@ -1,16 +1,31 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "api/theatre/",
-        include("theatre_service.urls",
-                namespace="theatre_service"
-                )
+        "api/theatre/", include(
+            "theatre_service.urls", namespace="theatre_service"
+        )
     ),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
