c139119b0908a5b744a7a54af70b5a0329ef6c0a
Eugene Rabenko <qwer09846@gmail.com>
2023-12-25 10:42:29 +0200

    move SECRET KEY variable to env file

--- theatre_service_api/settings.py
+++ theatre_service_api/settings.py
@@ -9,9 +9,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    "django-insecure-o(rsj%!jumf@xcy&+2=2z7y%dk4!7)g63ne29z#fvyr!bihwc2"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
 
